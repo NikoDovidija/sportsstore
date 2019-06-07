87d4bf8c8700d9ac416f44629570c21ca9326059
Nicke <nicke.friedman@gmail.com>
2019-06-07 17:44:30 +0200

    Added initial cart controller and views with models

--- SportsStoreCore/Controllers/ProductController.cs
+++ SportsStoreCore/Controllers/ProductController.cs
@@ -4,16 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStoreCore.Models;
+using SportsStoreCore.Models.ViewModels;
+
 namespace SportsStoreCore.Controllers
 {
     public class ProductController : Controller
     {
 
+
         IProductRepository repo;
+        public int PageSize = 4;
+
         public ProductController(IProductRepository productRepository)
         {
             repo = productRepository;
         }
-        public ViewResult List() => View(repo.Products);
+        public ViewResult List(string category,int productPage = 1)
+            //=> View(repo.Products
+            //.OrderBy(p => p.ProductID)
+            //.Skip((productPage - 1) * PageSize)
+            //.Take(PageSize));
+            => View(new ProductsListViewModel{
+                Products = 
+                repo.Products.OrderBy(p => p.ProductID)
+                .Where(p=> category == null || p.Category == category)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    TotalItems = category == null ? repo.Products.Count() : repo.Products.Where(e=>e.Category ==category).Count() ,
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize
+                },
+                CurrentCategory = category
+                
+            });
     }
 }
--- SportsStoreCore/Startup.cs
+++ SportsStoreCore/Startup.cs
@@ -23,6 +23,8 @@ namespace SportsStoreCore
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(
             Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddMemoryCache();
+            services.AddSession();
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
         }
@@ -34,14 +36,46 @@ namespace SportsStoreCore
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSession();
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvc(routes => {
                 routes.MapRoute(
-                name: "default",
-                template: "{controller=Product}/{action=List}/{id?}");
+                name: null,
+                template: "{category}/Page{productPage:int}",
+                defaults: new { controller = "Product", action = "List" }
+                );
+                routes.MapRoute(
+                name: null,
+                template: "Page{productPage:int}",
+                defaults: new
+                {
+                    controller = "Product",
+                    action = "List",
+                    productPage = 1
+                }
+                );
+                routes.MapRoute(
+                name: null,
+                template: "{category}",
+                defaults: new
+                {
+                    controller = "Product",
+                    action = "List",
+                    productPage = 1
+                }
+                );
+                routes.MapRoute(
+                name: null,
+                template: "",
+                defaults: new
+                {
+                    controller = "Product",
+                    action = "List",
+                    productPage = 1
+                });
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
             });
             SeedData.EnsurePopulated(app);
 
